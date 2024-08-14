db7091cb86d24dc4a07cb6372853129b072a0184
Aman Reja <amnmolla742@gmail.com>
2024-08-15 01:19:41 +0530

    fixed lag

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-const display = document.querySelector(".input-container");
+const display = document.querySelector(".hide");
 const search = document.querySelector(".icon");
 const okbutton = document.querySelector(".Ok-button");
 
 search.addEventListener("click", () => {
-  display.classList.toggle("hide");
+  display.classList.toggle("input-container");
 });
 okbutton.addEventListener("click", () => {
-  display.classList.toggle("hide");
+  display.classList.toggle("input-container");
 });
+
+// document.onclick = function (e) {
+//   if (e.target.classList == "display" && e.target.classList == "search") {
+//     display.classList.add("hide");
+//   }
+// };
+
+// document.onclick = function (e) {
+//   if (
+//     {!search.contains ? (e.target) `${display.classList.remove("hide")}}`
+
+//   )
+//     display.classList.remove("hide");
+//   menu.classList.remove("open-list");
+// };
 
 const list = document.querySelector(".list");
 const item = document.querySelectorAll(".item");
